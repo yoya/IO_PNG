0280be4dd452db84ed87ce7f96c70a4b7809267a
yoya <yoya@g.awm.jp>
2016-07-07 18:15:33 +0900

    - 先頭にある長さフィールドの値に合わせてチャンクの長さを切り詰める処理を追加 - CRC を計算し直す。

--- sample/pngcat.php
+++ sample/pngcat.php
@@ -8,7 +8,21 @@ function usage() {
 
 echo IO_PNG::SIGNATURE;
 foreach (array_slice($argv, 1) as $chunkfile) {
-    echo file_get_contents($chunkfile);
+    $chunk = file_get_contents($chunkfile);
+    $chunkLen = substr($chunk, 0, 4);
+    $tmp = unpack("N", $chunkLen);
+    $len = $tmp[1];
+    $chunkNameData = substr($chunk, 4, 4 + $len);
+    $crc = crc32($chunkNameData);
+    $chunkCRC = pack("N", $crc);
+    //
+    $chunkCRCorig = substr($chunk, 4 + 4 + $len, 4);
+    $tmp = unpack("N", $chunkCRCorig);
+    $origCRC= $tmp[1];
+    if ($origCRC !== $crc) {
+        fprintf(STDERR, "origCRC($origCRC) !== crc($crc)\n");
+    }
+    echo $chunkLen . $chunkNameData . $chunkCRC;
 }
 
 exit(0);
