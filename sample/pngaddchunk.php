83ac44072b6c79673f0683122990250e488ee3c2
Yoshihiro Yamazaki <yoya@awm.jp>
2017-08-24 15:52:19 +0900

    iCCP chunk insert to PNG

---
+++ sample/pngaddchunk.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'IO/PNG.php';
+
+$options = getopt("f:t:d:");
+
+function usage() {
+    echo "Usage: php pngaddchunk -f <pngfile> [-t <chunktype> -d <chunkdata>]".PHP_EOL;
+    echo "Usage: php pngaddchunk -f test.png".PHP_EOL;
+    echo "Usage: php pngaddchunk -f test.png -t iCCP -d sRGB.icc".PHP_EOL;
+}
+
+if ((isset($options['f']) === false) ||
+    (is_readable($options['f']) === false)) {
+    usage();
+    exit(1);
+}
+
+$pngfile = $options['f'];
+$pngdata = file_get_contents($pngfile);
+
+$png = new IO_PNG();
+$png->parse($pngdata);
+
+if (isset($options['t']) === false) {
+    foreach ($png->_chunkList as $idx => $chunk) {
+        echo $chunk['Name'].":";
+        if (is_string($chunk['Data'])) {
+            $chunkData = $chunk['Data'];
+            $chunkLen = strlen($chunkData);
+            echo "($chunkLen)";
+        }
+        echo PHP_EOL;
+    }
+} else {
+    $typeArg = $options['t'];
+    $dataArg = $options['d'];
+    switch ($typeArg) {
+    case "iCPP":
+        foreach ($png->_chunkList as $idx => $chunk) {
+            if (in_array($chunk["Name"], ["IHDR"]) === false) {
+                break;
+            }
+        }
+        $chunkData_head5 = "icc\0\0";
+        $iccData = file_get_contents($dataArg);
+        $chunk = ["Name" => "iCCP",
+                  "Data" => $chunkData_head5 . gzcompress($iccData)];
+        array_splice($png->_chunkList, $idx, 0, [$chunk]);
+        break;
+    default:
+        echo "Unknown type:$typeArg\n";
+        exit (1);
+    }
+    echo $png->build();
+}
