7e00e0130b394c49dc3fd6096cc4aa7e48562a9d
Yoshihiro Yamazaki <yoya@awm.jp>
2019-09-19 16:56:12 +0900

    PNG chunk delete tool

---
+++ sample/pngdelchunk.php
@@ -0,0 +1,57 @@
+<?php
+
+if (is_readable('vendor/autoload.php')) {
+    require 'vendor/autoload.php';
+} else {
+    require_once 'IO/PNG.php';
+}
+
+$options = getopt("f:t:");
+
+function usage() {
+    echo "Usage: php pngdelchunk -f <pngfile> [-t <chunktype>]".PHP_EOL;
+    echo "Usage: php pngdelchunk -f test.png".PHP_EOL;
+    echo "Usage: php pngdelchunk -f test.png -t tEXt".PHP_EOL;
+}
+
+if (isset($options['f']) === false) {
+    usage();
+    exit(1);
+}
+$pngfile = $options['f'];
+
+if ($pngfile === "-") {
+    $pngfile = "php://stdin";
+} else if (is_readable($pngfile) === false) {
+    usage();
+    exit(1);
+}
+
+$pngdata = file_get_contents($pngfile);
+
+$png = new IO_PNG();
+$png->parse($pngdata);
+
+if (isset($options['t']) === false) {
+    foreach ($png->_chunkList as $idx => $chunk) {
+        echo $chunk['Name'].":";
+        if (is_string($chunk['Data'])) {
+            $chunkData = $chunk['Data'];
+            $chunkLen = strlen($chunkData);
+            echo "($chunkLen)";
+        }
+        echo PHP_EOL;
+    }
+} else {
+    $typeArg = $options['t'];
+    $chunkList = [];
+    foreach ($png->_chunkList as $idx => $chunk) {
+        $chunkName = $chunk['Name'];
+        if ($chunkName === $typeArg)  {
+            continue; // delete chunk
+        }
+        $chunkList []= $chunk;
+    }
+    $png->_chunkList = $chunkList;
+    echo $png->build();
+}
