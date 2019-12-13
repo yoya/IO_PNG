4ac96876e22bac461713d29f4764456923d797a8
Yoshihiro Yamazaki <yoya@awm.jp>
2019-12-13 18:53:27 +0900

    refine dump format

--- IO/PNG.php
+++ IO/PNG.php
@@ -36,7 +36,7 @@ class IO_PNG {
             $crc = $bit->getUI32BE();
             $namedata_crc32 = crc32($chunkName.$data);
             if ($crc !== $namedata_crc32) {
-                fprintf(STDERR, "Warning: chunkName:$chunkName crc:$crc namedata_crc32=$namedata_crc32\n");
+                fprintf(STDERR, "Warning: chunkName:$chunkName crc:$crc namedata_crc32:$namedata_crc32\n");
             }
             switch ($chunkName) {
             case 'IHDR':
